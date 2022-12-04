44722b78f297bbb74def097fff02886b7c2fd9e2
Shidoengie <54602340+Shidoengie@users.noreply.github.com>
2022-12-04 17:38:56 +0000

    Added more features
    I have added : 
    remapable keys 
    running crouching
    Renamed variable names
    Along with coments for increased readablity

--- free_look_camera.gd
+++ free_look_camera.gd
@@ -1,13 +1,26 @@
 #Copyright © 2022 Marc Nahr: https://github.com/MarcPhi/godot-free-look-camera
 extends Camera3D
-
+#Key Exports for remapable keys
+@export var RunKey : InputEventKey = KeyToInputEventKey(KEY_SHIFT)
+@export var CrouchKey : InputEventKey = KeyToInputEventKey(KEY_C)
+@export var ForwardsKey : InputEventKey = KeyToInputEventKey(KEY_W)
+@export var BackwardsKey : InputEventKey = KeyToInputEventKey(KEY_S)
+@export var LeftKey : InputEventKey = KeyToInputEventKey(KEY_A)
+@export var RightKey : InputEventKey = KeyToInputEventKey(KEY_D)
+@export var DownKey : InputEventKey = KeyToInputEventKey(KEY_Q)
+@export var UpKey : InputEventKey = KeyToInputEventKey(KEY_E)
+#Mouse sensitivy
 @export_range(0, 10, 0.01) var sensitivity : float = 3
-@export_range(0, 1000, 0.1) var default_velocity : float = 5
-@export_range(0, 10, 0.01) var speed_scale : float = 1.17
-@export var max_speed : float = 1000
-@export var min_speed : float = 0.2
+@export_range(0, 1000, 0.1) var DefaultAcceleration : float = 5
+#Scales for affecting the base Acceleration
+@export_range(0,100,0.1) var RunScale : float = 2
+@export_range(0,100,0.1) var CrouchScale : float = 0.5
+@export_range(0, 10, 0.01) var SpeedScale : float = 1.17
 
-@onready var _velocity = default_velocity
+@export var MaxAccel : float = 1000
+@export var MinAccel : float = 0.2
+
+@onready var Acceleration = DefaultAcceleration
 
 func _input(event):
 	if not current:
@@ -24,15 +37,25 @@ func _input(event):
 			MOUSE_BUTTON_RIGHT:
 				Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED if event.pressed else Input.MOUSE_MODE_VISIBLE)
 			MOUSE_BUTTON_WHEEL_UP: # increase fly velocity
-				_velocity = clamp(_velocity * speed_scale, min_speed, max_speed)
+				Acceleration = clamp(Acceleration * SpeedScale, MinAccel, MaxAccel)
 			MOUSE_BUTTON_WHEEL_DOWN: # decrease fly velocity
-				_velocity = clamp(_velocity / speed_scale, min_speed, max_speed)
+				Acceleration = clamp(Acceleration / SpeedScale, MinAccel, MaxAccel)
 
 func _process(delta):
-	var direction = Vector3(
-		float(Input.is_key_pressed(KEY_D)) - float(Input.is_key_pressed(KEY_A)),
-		float(Input.is_key_pressed(KEY_E)) - float(Input.is_key_pressed(KEY_Q)), 
-		float(Input.is_key_pressed(KEY_S)) - float(Input.is_key_pressed(KEY_W))
+	var Direction = Vector3(
+		float(Input.is_key_pressed(RightKey.keycode)) - float(Input.is_key_pressed(LeftKey.keycode)),
+		float(Input.is_key_pressed(UpKey.keycode)) - float(Input.is_key_pressed(DownKey.keycode)), 
+		float(Input.is_key_pressed(BackwardsKey.keycode)) - float(Input.is_key_pressed(ForwardsKey.keycode))
 	).normalized()
-	
-	translate(direction * _velocity * delta)
+	if Input.is_key_pressed(RunKey.keycode):
+		translate(Direction * Acceleration * RunScale * delta)
+	elif Input.is_key_pressed(CrouchKey.keycode):
+		translate(Direction * Acceleration * CrouchScale * delta)
+	else:
+		translate(Direction * Acceleration * delta)
+
+#Converts Keycodes to InputKeyEvents mainly to be used for default key values
+func KeyToInputEventKey(key):
+	var newk = InputEventKey.new()
+	newk.keycode = key
+	return newk
